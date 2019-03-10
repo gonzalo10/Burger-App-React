3d68dd774f168e7ece5e1e383d00e3b75ce7f377
gonzalo10 <gonzalo.achiaga@gmail.com>
2019-03-10 13:39:13 +0100

    small fix

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -128,7 +128,10 @@ class BurgerBuilder extends Component {
         }
         let orderSummary = null;
 
-        let burger = this.state.error ? <p>Ingredients can´t be loaded</p>:<Spinner />
+        let burger = this.state.error ? 
+            <p>Ingredients can´t be loaded</p>
+            :
+            <Spinner />
 
         if(this.state.ingredients){
             burger = (
